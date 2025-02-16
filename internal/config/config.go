e32a3434ff5c2fc6706618fbb1bf27c20fcee58c
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-02-16 13:08:23 +0100

    feat: add a top-level config for AI
    Add a top level config to hold AI provider choice. Makes it easy to add
    more providers in future.

---
+++ internal/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"github.com/christian-gama/autocommit/internal/groq"
+	"github.com/christian-gama/autocommit/internal/openai"
+)
+
+// AIProvider specified for type safety
+type AIProvider string
+
+const (
+	OpenAI AIProvider = "openai"
+	Groq   AIProvider = "groq"
+)
+
+type Config struct {
+	Provider AIProvider
+	OpenAI   *openai.Config
+	Groq     *groq.Config
+}
